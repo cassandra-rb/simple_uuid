93f157a98c075237e654bd0037170a964f14e044
Ryan King <ryan@theryanking.com>
2010-01-20 18:51:22 -0800

    initial port

---
+++ test/test_uuid.rb
@@ -0,0 +1,35 @@
+require 'test/unit'
+require 'simple_uuid'
+
+class UUIDTest < Test::Unit::TestCase
+  def test_uuid_sort
+    ary = []
+    5.times { ary << UUID.new(Time.at(rand(2**31))) }
+    assert_equal ary.map { |_| _.seconds }.sort, ary.sort.map { |_| _.seconds }
+    assert_not_equal ary.sort, ary.sort_by {|_| _.to_guid }
+  end
+
+  def test_uuid_equality
+    uuid = UUID.new
+    assert_equal uuid, UUID.new(uuid)
+    assert_equal uuid, UUID.new(uuid.to_s)
+    assert_equal uuid, UUID.new(uuid.to_i)
+    assert_equal uuid, UUID.new(uuid.to_guid)
+  end
+
+  def test_uuid_error
+    assert_raises(TypeError) do
+      UUID.new("bogus")
+    end
+  end
+
+  def test_types_behave_well
+    assert !(UUID.new() == false)
+  end
+  
+  def test_uuid_casting_error
+    assert_raises(TypeError) do
+      UUID.new({})
+    end
+  end
+end
